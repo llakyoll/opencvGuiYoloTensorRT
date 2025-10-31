b3b191fac96a1db30c90dd4afab1eb3a4f7324a9
Ahmed <ahmedakyll@gmail.com>
2025-10-31 16:16:51 +0300

    Without gui yolo prediction with tensorrt and cuda.

---
+++ main.py
@@ -0,0 +1,48 @@
+import cv2
+import time
+from ultralytics import NAS, YOLO
+import torch
+
+# YOLOv8 modelini GPU'ya yükle
+model = YOLO("yolov8xInt8.engine")
+
+cap = cv2.VideoCapture(0)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# cap.set(cv2.CAP_PROP_FPS, 30)
+# out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'XVID'), 60, (640, 480))
+
+while cap.isOpened():
+    start_time = time.time()
+    success, im0 = cap.read()
+    if not success:
+        break
+
+    #im1 = cv2.resize(im0, (640, 480))
+
+    # Nesne takibi yap
+    results = model.predict(im0, device="cuda:0", verbose=False)
+
+    end_time = time.time()
+
+    if len(results) > 0:  # Eğer sonuçlar varsa
+        # Anotasyonlu görüntüyü al
+        annotated_frame = results[0].plot()
+
+        # FPS hesapla ve ekrana yaz
+        fps = 1 / (end_time - start_time)
+        cv2.putText(annotated_frame, f"FPS: {fps:.2f}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        # Video Dosyasını Kaydet
+        # out.write(annotated_frame)
+
+        # Anotasyonlu görüntüyü göster
+        cv2.imshow('Tracking Frame', annotated_frame)
+
+    # 'q' tuşuna basıldığında döngüyü bitir
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
---
+++ pytorchToEngine.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")  # Load a model
+# Fp32 quantization
+#model.export(format="engine", device="cuda:0")
+# Fp16 quantization
+# model.export(format="engine", device="cuda:0", half=True)
+# Int8 quantization
+model.export(format="engine", device="cuda:0", int8=True)
